bf6efcc3c866ed7ce9fe0e45c7ce2700fadba24d
flavianmdv <flavianmedvichi123@gmail.com>
2023-12-30 19:19:31 +0200

    3.2 work like shit

--- controller spring/controller-spring/src/main/java/org/launchcode/controller/EventController.java
+++ controller spring/controller-spring/src/main/java/org/launchcode/controller/EventController.java
@@ -1,14 +1,13 @@
 package org.launchcode.controller;
 
 
+import jakarta.validation.Valid;
 import org.launchcode.data.EventData;
 import org.launchcode.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("events")
@@ -21,13 +20,23 @@ public class EventController {
             return "events/index";
         }
 
+
+
         @GetMapping("create")
-        public String renderCreateEventForm() {
+        public String displayCreateEventForm(Model model) {
+            model.addAttribute("title", "Create Event");
             return "events/create";
         }
 
         @PostMapping("create")
-        public String processCreateEventForm(@ModelAttribute Event newEvent) {
+        public String processCreateEventForm(@ModelAttribute @Valid Event newEvent, Errors errors, Model model) {
+
+            if (errors.hasErrors()) {
+                model.addAttribute("title", "Create Event");
+                model.addAttribute("errorMsg", "Bad data!");
+                return "events/create";
+            }
+
             EventData.add(newEvent);
             return "redirect:/events";
         }
