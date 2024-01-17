3b254170a88fc9e1b1b1a59e51d332d3ae063036
flavianmdv <flavianmedvichi123@gmail.com>
2024-01-17 17:48:32 +0200

    final

--- controller spring/controller-spring/src/main/java/org/launchcode/controller/EventController.java
+++ controller spring/controller-spring/src/main/java/org/launchcode/controller/EventController.java
@@ -4,8 +4,11 @@ package org.launchcode.controller;
 import jakarta.validation.Valid;
 import org.launchcode.data.EventCategoryRepository;
 import org.launchcode.data.EventRepository;
+import org.launchcode.data.TagRepository;
 import org.launchcode.models.Event;
 import org.launchcode.models.EventCategory;
+import org.launchcode.models.Tag;
+import org.launchcode.models.dto.EventTagDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,6 +26,9 @@ public class EventController {
 
     @Autowired
     private EventCategoryRepository eventCategoryRepository;
+
+    @Autowired
+    private TagRepository tagRepository;
 
     @GetMapping
     public String displayEvents(@RequestParam(required = false) Integer categoryId, Model model) {
@@ -81,6 +87,53 @@ public class EventController {
         }
 
         return "redirect:/events";
+    }
+
+    @GetMapping("detail")
+    public String displayEventDetails(@RequestParam Integer eventId, Model model) {
+
+        Optional<Event> result = eventRepository.findById(eventId);
+
+        if (result.isEmpty()) {
+            model.addAttribute("title", "Invalid Event ID: " + eventId);
+        } else {
+            Event event = result.get();
+            model.addAttribute("title", event.getName() + " Details");
+            model.addAttribute("event", event);
+            model.addAttribute("tags", event.getTags());
+        }
+
+        return "events/detail";
+    }
+
+    @GetMapping("add-tag")
+    public String displayAddTagForm(@RequestParam Integer eventId, Model model){
+        Optional<Event> result = eventRepository.findById(eventId);
+        Event event = result.get();
+        model.addAttribute("title", "Add Tag to: " + event.getName());
+        model.addAttribute("tags", tagRepository.findAll());
+        EventTagDTO eventTag = new EventTagDTO();
+        eventTag.setEvent(event);
+        model.addAttribute("eventTag", eventTag);
+        return "events/add-tag.html";
+    }
+
+    @PostMapping("add-tag")
+    public String processAddTagForm(@ModelAttribute @Valid EventTagDTO eventTag,
+                                    Errors errors,
+                                    Model model){
+
+        if (!errors.hasErrors()) {
+            Event event = eventTag.getEvent();
+            Tag tag = eventTag.getTag();
+            if (!event.getTags().contains(tag)){
+                event.addTag(tag);
+                eventRepository.save(event);
+            }
+            return "redirect:detail?eventId=" + event.getId();
+        }
+
+        return "redirect:add-tag";
     }
 
 }
---
+++ controller spring/controller-spring/src/main/java/org/launchcode/data/TagRepository.java
@@ -0,0 +1,9 @@
+package org.launchcode.data;
+
+import org.launchcode.models.Tag;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends CrudRepository<Tag, Integer> {
+}
